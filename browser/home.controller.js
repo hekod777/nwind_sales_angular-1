38a21dbfc2426015c5642ce77a2418f165773c59
hekod777 <lifetimeinc@gmail.com>
2016-11-16 22:43:36 -0500

    angularization + tags
    open room, join room, open/join room are now angularized. and you can
    add tags to a video session. and the viewers can see the tags as well

--- browser/home.controller.js
+++ browser/home.controller.js
@@ -1,55 +1,14 @@
 angular.module('salesAngular')
   .controller('homeCtrl', function($scope){
 
+  $scope.disableOpenRoom = false;
+  $scope.disableJoinRoom = false;
+  $scope.disableOpenJoin = false;
+  $scope.tags = [];
+
+
   // ......................................................
   // .......................UI Code........................
-  // ......................................................
-  document.getElementById('open-room').onclick = function() {
-    this.disabled = true;
-    connection.open(document.getElementById('room-id').value);
-  };
-  document.getElementById('join-room').onclick = function() {
-    this.disabled = true;
-    connection.join(document.getElementById('room-id').value);
-  };
-  document.getElementById('open-or-join-room').onclick = function() {
-    this.disabled = true;
-    connection.openOrJoin(document.getElementById('room-id').value);
-  };
-  // ......................................................
-  // ................FileSharing/TextChat Code.............
-  // ......................................................
-  document.getElementById('share-file').onclick = function() {
-    var fileSelector = new FileSelector();
-    fileSelector.selectSingleFile(function(file) {
-      connection.send(file);
-    });
-  };
-  
-  document.getElementById('input-text-chat').onkeyup = function(e) {
-    if (e.keyCode != 13) return;
-
-    // removing trailing/leading whitespace
-    this.value = this.value.replace(/^\s+|\s+$/g, '');
-    if (!this.value.length) return;
-
-    connection.send(this.value);
-    appendDIV(this.value);
-    this.value = '';
-  };
-  var chatContainer = document.querySelector('.chat-output');
-
-  function appendDIV(event) {
-    var div = document.createElement('div');
-    div.innerHTML = event.data || event;
-    chatContainer.insertBefore(div, chatContainer.firstChild);
-    div.tabIndex = 0;
-    div.focus();
-
-    document.getElementById('input-text-chat').focus();
-  }
-  // ......................................................
-  // ..................RTCMultiConnection Code.............
   // ......................................................
   var connection = new RTCMultiConnection();
   //free signal server
@@ -65,9 +24,103 @@ angular.module('salesAngular')
     OfferToReceiveAudio: true,
     OfferToReceiveVideo: true
   };
+
+
+  var videoContainer = document.getElementById('video-container');
+
   connection.onstream = function(event) {
-    document.body.appendChild(event.mediaElement);
+    //document.body.appendChild(event.mediaElement);
+
+    var div = document.createElement('div');
+    div.className = 'video-div';
+    div.appendChild(event.mediaElement);
+
+    var h2= document.createElement('h2');
+    h2.innerHTML = event.extra.tags;
+    div.appendChild(h2);
+
+    videoContainer.appendChild(div);
+
   };
-  connection.onmessage = appendDIV;
+
+  connection.extra = {
+    category:'',
+    tags:[],
+  }
+
+
+
+  $scope.addTag = function (){
+    connection.extra.tags.push($scope.tag);
+    $scope.tag = null;
+    $scope.tags = connection.extra.tags;
+    console.log(connection.extra.tags);
+
+  }
+
+
+
+
+  // document.getElementById('open-room').onclick = function() {
+  //   this.disabled = true;
+  //   connection.open(document.getElementById('room-id').value);
+  // };
+
+  $scope.openRoom = function() {
+    $scope.disableOpenRoom = true;
+    connection.open($scope.roomname);
+  };
+
+  $scope.joinRoom = function() {
+    $scope.disableJoinRoom = true;
+    connection.join($scope.roomname);
+  };
+
+  $scope.joinOpen = function() {
+    $scope.disableOpenJoin = true;
+    connection.openOrJoin($scope.roomname);
+  };
+
+
+
+
+
+  // ......................................................
+  // ................FileSharing/TextChat Code.............
+  // ......................................................
+  // document.getElementById('share-file').onclick = function() {
+  //   var fileSelector = new FileSelector();
+  //   fileSelector.selectSingleFile(function(file) {
+  //     connection.send(file);
+  //   });
+  // };
+  
+  // document.getElementById('input-text-chat').onkeyup = function(e) {
+  //   if (e.keyCode != 13) return;
+
+  //   // removing trailing/leading whitespace
+  //   this.value = this.value.replace(/^\s+|\s+$/g, '');
+  //   if (!this.value.length) return;
+
+  //   connection.send(this.value);
+  //   appendDIV(this.value);
+  //   this.value = '';
+  // };
+  // var chatContainer = document.querySelector('.chat-output');
+
+  // function appendDIV(event) {
+  //   var div = document.createElement('div');
+  //   div.innerHTML = event.data || event;
+  //   chatContainer.insertBefore(div, chatContainer.firstChild);
+  //   div.tabIndex = 0;
+  //   div.focus();
+
+  //   document.getElementById('input-text-chat').focus();
+  // }
+  // ......................................................
+  // ..................RTCMultiConnection Code.............
+  // ......................................................
+  
+  // connection.onmessage = appendDIV;
 
 });
